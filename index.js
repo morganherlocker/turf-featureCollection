80044a1249e9c4a240e0230964a702d725d25365
Tom MacWright <tom@macwright.org>
2015-01-07 15:26:32 -0500

    JSdocs.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/**
+ * Creates a {@link FeatureCollection}
+ *
+ * @module turf/featurecollection
+ * @param {Feature[]} features
+ * @returns {FeatureCollection} output
+ * @example
+ * var pt1 = turf.point(-75.343, 39.984, {name: 'Location A'})
+ * var pt2 = turf.point(-75.833, 39.284, {name: 'Location B'})
+ * var pt3 = turf.point(-75.534, 39.123, {name: 'Location C'})
+ * var fc = turf.featurecollection([pt1, pt2, pt3])
+ * console.log(fc)
+ */
 module.exports = function(features){
   var fc = {
     "type": "FeatureCollection",
