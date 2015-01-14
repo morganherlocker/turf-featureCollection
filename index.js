f0e87dc7c2607019b89f73a8bdb06bcf566ec75e
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-13 17:28:39 -0800

    Small styling fixes, rpl-ize example

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
  * Creates a {@link FeatureCollection}
  *
  * @module turf/featurecollection
- * @param {Feature[]} features
- * @returns {FeatureCollection} output
+ * @param {Feature} features input Features
+ * @returns {FeatureCollection} a FeatureCollection of input features
  * @example
- * var pt1 = turf.point(-75.343, 39.984, {name: 'Location A'})
- * var pt2 = turf.point(-75.833, 39.284, {name: 'Location B'})
- * var pt3 = turf.point(-75.534, 39.123, {name: 'Location C'})
- * var fc = turf.featurecollection([pt1, pt2, pt3])
- * console.log(fc)
+ * var pt1 = turf.point(-75.343, 39.984, {name: 'Location A'});
+ * var pt2 = turf.point(-75.833, 39.284, {name: 'Location B'});
+ * var pt3 = turf.point(-75.534, 39.123, {name: 'Location C'});
+ * var fc = turf.featurecollection([pt1, pt2, pt3]);
+ *
+ * //=fc
  */
 module.exports = function(features){
   var fc = {
