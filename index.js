fc9c03e2d0d6aa6aa927b8e4aaeedd969570d3e3
Tom MacWright <tom@macwright.org>
2015-01-21 12:10:53 -0800

    Fix point form

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
  * @param {Feature} features input Features
  * @returns {FeatureCollection} a FeatureCollection of input features
  * @example
- * var pt1 = turf.point(-75.343, 39.984, {name: 'Location A'});
- * var pt2 = turf.point(-75.833, 39.284, {name: 'Location B'});
- * var pt3 = turf.point(-75.534, 39.123, {name: 'Location C'});
+ * var pt1 = turf.point([-75.343, 39.984], {name: 'Location A'});
+ * var pt2 = turf.point([-75.833, 39.284], {name: 'Location B'});
+ * var pt3 = turf.point([-75.534, 39.123], {name: 'Location C'});
  * var fc = turf.featurecollection([pt1, pt2, pt3]);
  *
  * //=fc
  */
 module.exports = function(features){
   return {
-    "type": "FeatureCollection",
-    "features": features
+    type: "FeatureCollection",
+    features: features
   };
-}
+};
