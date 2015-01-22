0ed7894dda138fa760432d1f801e894dd401b540
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-21 17:07:26 -0800

    Conform to turf doc style guide

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /**
- * Creates a {@link FeatureCollection}
+ * Takes one or more {@link Feature|Features} and creates a {@link FeatureCollection}
  *
  * @module turf/featurecollection
  * @param {Feature} features input Features
  * @returns {FeatureCollection} a FeatureCollection of input features
  * @example
- * var pt1 = turf.point([-75.343, 39.984], {name: 'Location A'});
- * var pt2 = turf.point([-75.833, 39.284], {name: 'Location B'});
- * var pt3 = turf.point([-75.534, 39.123], {name: 'Location C'});
- * var fc = turf.featurecollection([pt1, pt2, pt3]);
+ * var features = [
+ *  turf.point([-75.343, 39.984], {name: 'Location A'}),
+ *  turf.point([-75.833, 39.284], {name: 'Location B'}),
+ *  turf.point([-75.534, 39.123], {name: 'Location C'})
+ * ];
+ *
+ * var fc = turf.featurecollection(features);
  *
  * //=fc
  */
